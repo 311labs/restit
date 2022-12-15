b1ffc2e7d4bdebde2de24b9aee40423a7e237e6c
Ian Starnes <ians@311labs.com>
2022-12-15 11:16:45 -0800

    fixing missing rest endpoints for inbox

--- inbox/rpc.py
+++ inbox/rpc.py
@@ -1,18 +1,47 @@
 from rest import decorators as rd
 from rest import views as rv
 from rest import helpers as rh
-from . import models as mailbox
+from . import models as inbox
 from .handlers import SES_HANDLERS
 
 
-@rd.url(r'^mailbox$')
-@rd.url(r'^mailbox/(?P<pk>\d+)$')
-def rest_on_mailbox(request, pk=None):
-    return mailbox.Message.on_rest_request(request, pk)
+@rd.url(r'^inbox$')
+@rd.url(r'^inbox/(?P<pk>\d+)$')
+@rd.login_required
+def rest_inbox(request, pk=None):
+    return inbox.Mailbox.on_rest_request(request, pk)
+
+
+@rd.url(r'^message$')
+@rd.url(r'^message/(?P<pk>\d+)$')
+@rd.login_required
+def rest_message(request, pk=None):
+    return inbox.Message.on_rest_request(request, pk)
+
+
+@rd.url(r'^message/attachment$')
+@rd.url(r'^message/attachment/(?P<pk>\d+)$')
+@rd.login_required
+def rest_attachment(request, pk=None):
+    return inbox.Attachment.on_rest_request(request, pk)
+
+
+@rd.url(r'^bounced$')
+@rd.url(r'^bounced/(?P<pk>\d+)$')
+@rd.login_required
+def rest_bounced(request, pk=None):
+    return inbox.Bounce.on_rest_request(request, pk)
+
+
+@rd.url(r'^complaint$')
+@rd.url(r'^complaint/(?P<pk>\d+)$')
+@rd.login_required
+def rest_complaint(request, pk=None):
+    return inbox.Complaint.on_rest_request(request, pk)
 
 
 @rd.urlPOST(r'^ses/incoming$')
-def rest_on_ses_incoming(request):
+def rest_ses_incoming(request):
     msg_type = request.DATA.get("Type")
     handler = SES_HANDLERS.get(msg_type, None)
     if not handler:
