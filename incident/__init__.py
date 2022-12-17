f272a1268599d9dc013da34fcf8f7b7a8099536e
Ian Starnes <ians@311labs.com>
2022-12-16 22:27:50 -0800

    new admin incident pages

---
+++ incident/__init__.py
@@ -0,0 +1,8 @@
+from objict import objict
+
+
+def event(category, description, level=10, **kwargs):
+    from taskqueue.models import Task
+    data = objict(category=category, description=description, level=level)
+    data.update(kwargs)
+    Task.Publish("incident", "new_event", channel="tq_app_handler", data=data)
