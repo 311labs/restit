70da6dcbf2562afe2314b9724f9c6a83667b1c55
Ian Starnes <ians@311labs.com>
2022-12-16 22:35:06 -0800

    new admin incident pages

--- incident/tq.py
+++ incident/tq.py
@@ -6,16 +6,9 @@ Task.Publish("myapp", "on_tq_test")
 # from datetime import datetime, timedelta
 # from auditlog.models import PersistentLog
 # from django.conf import settings
+from incident.models import Event
 
 
-# def on_cleanup(task):
-#     # cleanup log files in the var directory
-#     if AUDITLOG_PRUNE_DAYS:
-#         before = datetime.now() - timedelta(days=90)
-#         # remove all rest logs older then 6 months
-#         task.log("purging old persistent rest logs")
-#         PersistentLog.objects.filter(when__lte=before, component__in=["rest", "action"]).delete()
-#         before = datetime.now() - timedelta(days=AUDITLOG_PRUNE_DAYS * 2)
-#         task.log("purging old persistent logs")
-#         PersistentLog.objects.filter(when__lte=before).exclude(component="account.Member").delete()
-
+def new_event(task):
+    Event.createFromDict(None, task.data)
+    task.completed()
